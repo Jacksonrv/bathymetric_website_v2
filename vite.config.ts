f24e3b8a7fa8f5c5d1d530447473879459910717
Jackson Vaughn <kw24171@bristol.ac.uk>
2025-06-27 09:21:45 +0100

    Fix redirect

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  publicDir: 'public',
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        redirects: resolve(__dirname, 'public/_redirects'),
-      }
-    }
-  }
 });
